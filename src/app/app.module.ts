d71531f24edb5a6bdb4625aba1f9e1b82ca3fd28
pukowtom <tomas.pukowiec@tieto.com>
2022-02-17 09:19:36 +0100

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
-import { NgModule, Injector } from '@angular/core';
+import { NgModule, Injector, DoBootstrap } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { MyElementComponent } from './my-element/my-element.component';
-import {createCustomElement} from '@angular/elements';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
-  declarations: [
-    MyElementComponent,
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [MyElementComponent],
+  imports: [BrowserModule],
   providers: [],
   //bootstrap: [AppComponent],
   entryComponents: [MyElementComponent],
 })
-export class AppModule {
+export class AppModule implements DoBootstrap {
   constructor(private injector: Injector) {}
+
   ngDoBootstrap() {
-    const el = createCustomElement(MyElementComponent, { injector: this.injector });
+    const el = createCustomElement(MyElementComponent, {
+      injector: this.injector,
+    });
     customElements.define('my-element', el);
   }
 }
